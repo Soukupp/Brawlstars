14ac3216af282aaedfb8aa5f9a374d463bf3b015
WinstonLiyt <2150260@tongji.edu.cn>
2022-05-21 19:43:55 +0800

    地图方面改版，增加了MapLayer专门处理地图，增加了游戏进行中的音量调节界面

---
+++ Classes/GameSettingsScene.cpp
@@ -0,0 +1,292 @@
+//作者 : 王鹏
+//日期 : 2022-5-14
+
+//作者 ：李元特
+//日期 : 2022-5-21
+//实现 ：对接游戏界面，音量调节
+
+
+#include "GameSettingsScene.h"
+#include "GameScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+static bool ifShowPlay = true;          //是否播放音乐
+static bool ifShowStates = false;       //是否显示FPS
+
+/****************************
+* Name ：GameSettingsScene::createScene
+* Summary ：创建场景
+* return ：场景类指针
+****************************/
+Scene* GameSettingsScene::createScene()
+{
+    return GameSettingsScene::create();
+}
+/****************************
+* Name ：problemLoading
+* Summary ：错误打印
+* return ：
+****************************/
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in SettingsScene.cpp\n");
+}
+/****************************
+* Name ：GameSettingsScene::init
+* Summary ：主菜单初始化
+* return ：初始化成功与否
+****************************/
+bool GameSettingsScene::init()
+{
+    if (!Scene::init())
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /*=====================创建返回按钮开始======================*/
+
+    //创建返回按钮
+    auto settingsBackItem = MenuItemImage::create(
+        "backSettingsNormal.png",
+        "backSettingsSelected.png",
+        CC_CALLBACK_1(GameSettingsScene::settingsBackToGameCallback, this));
+
+    if (settingsBackItem == nullptr ||
+        settingsBackItem->getContentSize().width <= 0 ||
+        settingsBackItem->getContentSize().height <= 0)
+    {//错误处理
+        problemLoading("'backSettingsNormal.png' and 'backSettingsSelected.png'");
+    }
+    else
+    {//设置位置
+        float x = SETTINGS_BACK_ITEM_POSITION_X;
+        float y = SETTINGS_BACK_ITEM_POSITION_Y;
+        settingsBackItem->setPosition(Vec2(x, y));
+    }
+    //创建返回菜单
+    auto backMenu = Menu::create(settingsBackItem, NULL);
+    backMenu->setPosition(Vec2::ZERO);
+    this->addChild(backMenu, 2);
+    /*=====================创建关闭按钮结束====================*/
+
+    /*=====================创建标题开始======================*/
+
+    auto settingsNameLabel = Label::createWithTTF(
+        "SETTINGS",
+        "fonts/PixeloidSans.ttf",
+        SETTINGS_NAME_LABEL_SIZE
+    );//创建文本
+    if (settingsNameLabel == nullptr)
+    {
+        problemLoading("'fonts/PixeloidSans.ttf'");
+    }
+    else
+    {
+        const Color4B settingsNameLabelColor(255, 245, 245, 255);//创建4B颜色
+        settingsNameLabel->setTextColor(settingsNameLabelColor);
+        settingsNameLabel->setPosition(
+            Vec2(SETTINGS_NAME_LABEL_POSITION_X,
+                SETTINGS_NAME_LABEL_POSITION_Y)
+        );
+
+        this->addChild(settingsNameLabel, 1);
+    }
+    /*=====================创建标题结束======================*/
+
+    /*===================创建滑动条开始======================*/
+
+    _displayedPercentage = Text::create("0", "fonts/PixeloidSans.ttf", 27);          //_displayedPercentage 用于显示滑块拖动后所占比例
+    _displayedPercentage->setPosition(Vec2(SETTINGS_SETTINGSMENU_POSITION_X, SETTINGS_SETTINGSMENU_POSITION_Y * 0.5));
+
+    auto musicSlider = Slider::create();
+
+    musicVolume = UserDefault::getInstance()->getFloatForKey("musicVolume");   //musicVolume 用于记录音量大小（百分制）
+    musicVolume = 50.0f;                                                        //定义初始值为50
+    _displayedPercentage->setString(StringUtils::format("Percent %d", 50));
+
+    musicSlider->setPercent(musicVolume);
+    musicSlider->loadBarTexture("progressFrame.png");
+    musicSlider->loadProgressBarTexture("progressBlock.png");
+    musicSlider->setPosition(Vec2(SETTINGS_SETTINGSMENU_POSITION_X, SETTINGS_SETTINGSMENU_POSITION_Y));
+    musicSlider->addEventListener(CC_CALLBACK_2(GameSettingsScene::sliderEvent, this));
+    musicSlider->setScale(0.5);
+
+    this->addChild(_displayedPercentage, 3);
+    this->addChild(musicSlider, 2);
+
+    /*===================创建滑动条结束====================*/
+
+    /*===================创建标签开始========================*/
+
+    Label* settingsMusicLabel = Label::create("MUSIC SETTING", "fonts/PixeloidSans.ttf", 27);
+    settingsMusicLabel->setPosition(SETTINGS_SETTINGSMUSICLABEL_POSITION_X, SETTINGS_SETTINGSMUSICLABEL_POSITION_Y);
+    const Color4B settingsMusicLabelColor(255, 140, 15, 255);//创建4B颜色
+    settingsMusicLabel->setTextColor(settingsMusicLabelColor);
+
+    this->addChild(settingsMusicLabel, 3);
+
+    /*===================创建标签结束=========================*/
+
+    /*===================创建菜单开始========================*/
+
+    auto musicOn = MenuItemImage::create("musicOn.png", "musicOn.png");
+    auto musicOff = MenuItemImage::create("musicOff.png", "musicOff.png");
+
+    MenuItemToggle* musicOnOrOff = MenuItemToggle::createWithTarget(this,
+        menu_selector(GameSettingsScene::settingsPlayCallBack), musicOn, musicOff, NULL);
+    //显示音乐开始或静音图标
+
+    if (ifShowPlay)
+    {
+        musicOnOrOff->setSelectedIndex(0);
+        _displayedMusicStates->setString(StringUtils::format("MUSIC ON"));
+    }
+    else
+    {
+        musicOnOrOff->setSelectedIndex(1);
+        _displayedMusicStates->setString(StringUtils::format("MUSIC OFF"));
+    }
+
+    _displayedMusicStates->setTextColor(settingsMusicLabelColor);
+    _displayedMusicStates->setPosition(Vec2(SETTINGS_SETTINGMUSICSTATES_POSITION_X, SETTINGS_SETTINGMUSICSTATES_POSITION_Y));
+
+
+
+    auto FPSOn = MenuItemImage::create("FPSOn.png", "FPSOn.png");
+    auto FPSOff = MenuItemImage::create("FPSOff.png", "FPSOff.png");
+
+    MenuItemToggle* FPSOnOrOff = MenuItemToggle::createWithTarget(this,
+        menu_selector(SettingsScene::settingsFPSCallBack), FPSOn, FPSOff, NULL);
+    //显示FPS显示或隐藏图标
+
+
+    FPSOnOrOff->setPosition(Vec2(SETTINGS_SETTINGFPSSTATES_POSITION_X, SETTINGS_SETTINGFPSSTATES_POSITION_Y));
+
+    if (ifShowStates)
+    {
+        FPSOnOrOff->setSelectedIndex(0);
+        _displayedFPSStates->setString(StringUtils::format("DISPLAY FPS"));
+    }
+    else
+    {
+        FPSOnOrOff->setSelectedIndex(1);
+        _displayedFPSStates->setString(StringUtils::format("CONCEAL FPS"));
+    }
+
+    _displayedFPSStates->setTextColor(settingsMusicLabelColor);
+    _displayedFPSStates->setPosition(Vec2(SETTINGS_SETTINGSFPSLABEL_POSITION_X, SETTINGS_SETTINGSFPSLABEL_POSITION_Y));
+
+
+    Menu* GameSettingsMenu = Menu::create(musicOnOrOff, FPSOnOrOff, NULL);
+
+    this->addChild(GameSettingsMenu, 3);
+    addChild(_displayedMusicStates, 3);
+    addChild(_displayedFPSStates, 3);
+
+    /*===================创建菜单结束========================*/
+
+
+
+
+    /*====================创建背景图开始======================*/
+    auto background = Sprite::create("GameSettingsBackground.png");
+    if (background == nullptr)
+    {
+        problemLoading("'GameSettingsBackground.png'");
+    }
+    else
+    {
+        //将背景图放在中央
+        background->setPosition(Vec2(SETTINGS_BACKGROUND_POSITION_X,
+            SETTINGS_BACKGROUND_POSITION_Y));
+
+        this->addChild(background, 0);
+    }
+    /*=====================创建背景图结束======================*/
+    return true;
+}
+
+/****************************
+* Name ：GameSettingsScene::settingsBackCallback
+* Summary ：设置返回按钮回调
+* return ：
+****************************/
+void GameSettingsScene::settingsBackToGameCallback(Ref* pSender)
+{
+    auto GS = GameScene::createScene();
+    Director::getInstance()->popScene();//过场动画设计
+}
+
+/****************************
+* Name ：GameSettingsScene::sliderEvent
+* Summary ：滑动事件监听
+* return ：
+* ***************************/
+void GameSettingsScene::sliderEvent(Ref* pSender, Slider::EventType type)
+{
+    if (type == Slider::EventType::ON_PERCENTAGE_CHANGED)
+    {
+        Slider* slider = dynamic_cast<Slider*>(pSender);
+        int percentVolume = slider->getPercent();
+
+        log(percentVolume);
+        CocosDenshion::SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(2 * float(percentVolume) / 100);
+        //CocosDenshion::SimpleAudioEngine::sharedEngine()->setBackgroundMusicVolume(CocosDenshion::SimpleAudioEngine::sharedEngine()->getBackgroundMusicVolume() + float(percentVolume) / 100);
+
+        UserDefault::getInstance()->setFloatForKey("musicVolume", percentVolume);
+
+        _displayedPercentage->setString(StringUtils::format("Percent %d", percentVolume));   //显示所占百分比
+
+    }
+}
+/****************************
+* Name ：GameSettingsScene::settingsPlayCallBack
+* Summary  回调函数
+* return ：
+* ***************************/
+
+void GameSettingsScene::settingsPlayCallBack(Ref* pSender)
+{
+
+
+    if (CocosDenshion::SimpleAudioEngine::sharedEngine()->isBackgroundMusicPlaying())
+    {
+        CocosDenshion::SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+        ifShowPlay = false;
+        _displayedMusicStates->setString(StringUtils::format("MUSIC OFF"));
+    }
+    else
+    {
+        CocosDenshion::SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+        ifShowPlay = true;
+        _displayedMusicStates->setString(StringUtils::format("MUSIC ON"));
+    }
+}
+/****************************
+* Name ：GameSettingsScene::settingsFPSCallBack
+* Summary  回调函数
+* return ：
+* ***************************/
+
+void GameSettingsScene::settingsFPSCallBack(Ref* pSender)
+{
+    auto director = Director::getInstance();
+    if (director->isDisplayStats())
+    {
+        director->setDisplayStats(false);
+        ifShowStates = false;
+        _displayedFPSStates->setString(StringUtils::format("CONCEAL FPS"));
+    }
+    else
+    {
+        director->setDisplayStats(true);
+        ifShowStates = true;
+        _displayedFPSStates->setString(StringUtils::format("DISPLAY FPS"));
+    }
+}
---
+++ Classes/MapLayer.cpp
@@ -0,0 +1,224 @@
+//作者 : 李元特
+//日期 : 2022-5-20
+
+#include "MapLayer.h"
+#include "GameOverScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+using namespace CocosDenshion;
+
+
+/****************************
+* Name ：problemLoading
+* Summary ：错误打印
+* return ：
+****************************/
+static void problemLoading(const char* filename)
+{
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+
+/****************************
+* Name ：MapLayer::init
+* Summary ：地图层初始化
+* return ：初始化成功与否
+****************************/
+bool MapLayer::init()
+{
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+
+	/*======================创建地图开始=======================*/
+	//log("1");
+	_tileMap = TMXTiledMap::create("map/MiddleMap.tmx");
+	// 用老地图MiddleMap可以，不知道为啥新地图不可以，还在研究中......
+	// 	_tileMap = TMXTiledMap::create("map/Map.tmx");
+
+
+	addChild(_tileMap, 0, 100);
+	//log("2");
+	TMXObjectGroup* group = _tileMap->getObjectGroup("objects");
+	ValueMap spawnPoint = group->getObject("Colt");  // 从地图读取游戏人物的位置，游戏人物Colt
+
+	//ValueMap spawnPoint = group->getObject("player");  // 新地图应该是player
+
+	float _playerX = spawnPoint["x"].asFloat();
+	float _playerY = spawnPoint["y"].asFloat();
+	/*=====================创建角色开始========================*/
+	_player = Player::create("Colt.png");
+	_player->setPosition(Vec2(_playerX, _playerY));
+	_player->initPlayer(100, 2, 3, 4.0f, 5.0f);
+	addChild(_player, 2, 200);
+
+	_weapon = Weapon::create("weaponWeapon.png");
+	_weapon->setAnchorPoint(
+		Vec2(PLAYER_WEAPON_ANCHOR_POSITION_X_WHEN_RIGHT,
+			PLAYER_WEAPON_ANCHOR_POSITION_Y));
+	addChild(_weapon, 2, 200);
+	_player->keepWeapon(_weapon);
+	/*=====================创建角色结束========================*/
+	setViewpointCenter(_player->getPosition());
+
+	_collidable = _tileMap->getLayer("collidable");  //障碍物collidable
+	_collidable->setVisible(false);  // 对应透明度
+
+	setTouchEnabled(true);  // 开启触摸，必须继承于layer
+	setTouchMode(Touch::DispatchMode::ONE_BY_ONE);
+	/*======================创建地图结束=======================*/
+
+	return true;
+}
+
+/****************************
+* Name ：MapLayer::onTouchBegan
+* Summary ：触摸开始
+* return ：是否执行
+****************************/
+bool MapLayer::onTouchBegan(Touch* touch, Event* event)
+{
+	log("onTouchBegan"); //日志
+	return true;
+}
+
+/****************************
+* Name ：MapLayer::onTouchMoved
+* Summary ：触摸移动
+* return ：是否执行
+****************************/
+void MapLayer::onTouchMoved(Touch* touch, Event* event)
+{
+	log("onTouchMoved"); //日志
+}
+
+/****************************
+* Name ：MapLayer::onTouchEnded
+* Summary ：触摸结束
+* return ：是否执行
+****************************/
+void MapLayer::onTouchEnded(Touch* touch, Event* event)
+{
+	log("onTouchEnded");  //日志 
+
+	/*================获取触摸点的坐标，并转化为当前层模型坐标系==================*/
+	Vec2 touchLocation = touch->getLocation();  //获得在OpenGL坐标
+	touchLocation = this->convertToNodeSpace(touchLocation);  //转换为当前层的模型坐标系
+
+	Vec2 playerPos = _player->getPosition();  // 获取玩家位置坐标
+
+	Vec2 diff = touchLocation - playerPos;    // 获取上述两者坐标差 
+
+	/*=============================坐标获取结束================================*/
+
+
+	/*=======================通过鼠标控制人物走动开始===========================*/
+	if (abs(diff.x) > abs(diff.y)) {
+		if (diff.x > 0) {
+			playerPos.x += _tileMap->getTileSize().width;
+			//_player->runAction(FlipX::create(false));
+			_player->runFlipxWithWeapon(false, _weapon);
+		}
+		else {
+			playerPos.x -= _tileMap->getTileSize().width;
+			//_player->runAction(FlipX::create(true));
+			_player->runFlipxWithWeapon(true, _weapon);
+		}
+	}
+	else {
+		if (diff.y > 0) {
+			playerPos.y += _tileMap->getTileSize().height;
+		}
+		else {
+			playerPos.y -= _tileMap->getTileSize().height;
+		}
+	}
+	this->setPlayerPosition(playerPos);   // 判断可不可以走动
+	/*=======================通过鼠标控制人物走动结束===========================*/
+}
+
+/****************************
+* Name ：MapLayer::setPlayerPosition
+* Summary ：是否走动
+* return ：无
+****************************/
+void MapLayer::setPlayerPosition(Vec2 position)
+{
+	// 读取坐标
+	Vec2 tileCoord = this->tileCoordFromPosition(position);  //从像素点坐标转化为瓦片坐标
+
+	int tileGid = _collidable->getTileGIDAt(tileCoord);   //获得瓦片的GID
+
+	// 碰撞检测
+	if (tileGid > 0) {
+		Value prop = _tileMap->getPropertiesForGID(tileGid);
+		ValueMap propValueMap = prop.asValueMap();
+
+		std::string collision = propValueMap["Collidable"].asString();
+		// 元素+true
+		if (collision == "true") { //碰撞检测成功
+			CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("sound/empty.wav");
+			return;
+		}
+	}
+	//移动精灵
+	//_player->hitPlayer(5);//对角色造成攻击
+	_player->setPositionWithAll(position, _weapon);
+	//滚动地图
+	this->setViewpointCenter(_player->getPosition());
+}
+
+/****************************
+* Name ：MapLayer::tileCoordFromPosition
+* Summary ：从像素点坐标转化为瓦片坐标
+* return ：Vec2(x,y)
+****************************/
+Vec2 MapLayer::tileCoordFromPosition(Vec2 pos)
+{
+	int x = pos.x / _tileMap->getTileSize().width;
+	int y = ((_tileMap->getMapSize().height * _tileMap->getTileSize().height) - pos.y) / _tileMap->getTileSize().height;
+	return Vec2(x, y);
+}
+
+/****************************
+* Name ：MapLayer::setViewpointCenter
+* Summary ：滚动地图
+* return ：无
+****************************/
+void MapLayer::setViewpointCenter(Vec2 position)
+{
+	log("setViewpointCenter"); //日志
+
+	log("position (%f ,%f) ", position.x, position.y);
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	//可以防止，视图左边超出屏幕之外。
+	int x = MAX(position.x, visibleSize.width / 2);
+	int y = MAX(position.y, visibleSize.height / 2);
+	//可以防止，视图右边超出屏幕之外。
+	x = MIN(x, (_tileMap->getMapSize().width * _tileMap->getTileSize().width)
+		- visibleSize.width / 2);
+	y = MIN(y, (_tileMap->getMapSize().height * _tileMap->getTileSize().height)
+		- visibleSize.height / 2);
+
+	//屏幕中心点
+	Vec2 pointA = Vec2(visibleSize.width / 2, visibleSize.height / 2);
+
+	//使精灵处于屏幕中心，移动地图目标位置
+	Vec2 pointB = Vec2(x, y);
+	log("目标位置 (%f ,%f) ", pointB.x, pointB.y);
+
+	//地图移动偏移量
+	Vec2 offset = pointA - pointB;
+	log("offset (%f ,%f) ", offset.x, offset.y);
+	this->setPosition(offset);
+
+}
+
